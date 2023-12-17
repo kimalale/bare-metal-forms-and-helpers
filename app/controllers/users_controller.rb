79388602587a5b9e74c78ec8242b267fb25a69e3
Kim Malale <kimalale@hotmail.com>
2023-12-17 22:59:21 +0200

    Update CRUD fucns

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,34 @@ class UsersController < ApplicationController
         
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def index
+        @users = User.all
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+
+        if @user.update(user_params)
+        redirect_to @user
+        else
+        render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+
+        redirect_to root_path, status: :see_other
+    end
     def user_params
         params.require(:user).permit(:username,:email,:password)
     end
